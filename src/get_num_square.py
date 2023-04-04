3e579cdd116ac8b8f9262b8d6ec8feea8f07d6ab
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-04-04 17:49:19 +0900

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -3,11 +3,9 @@ num=os.environ.get("INPUT_NUM")
 if num:
   try:
     num=int(num)
-  except Exception:
-    exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
-    
+   except Exception:
+    exit('ERROR: the INPUT_NUM provided ("{}") is not an integar'.format(num))
 else:
   num=1
   
 print(f"::set-output name=num_squared::{num**2}")
-  
